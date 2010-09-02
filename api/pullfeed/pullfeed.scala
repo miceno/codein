70dfb7c936cccf0a4aa1b42e5ce1bd735c963878
miceno <orestes@acm.org>
2010-09-02 17:12:20 +0200

    Improved parameter syntax and parsing.

--- api/pullfeed/pullfeed.scala
+++ api/pullfeed/pullfeed.scala
@@ -5,13 +5,12 @@
 
 import scala.xml._
 import scala.io.Source
-import Codein._
+import codein._
+import scala.collection.mutable._
 
 object PullFeed {
 
-
-
-  /**
+  /*
    * @param args the command line arguments
    *    String: source to read
    *    String: mapping expression: how to map input xpath fields to Codein API fields. The mapping is like:
@@ -19,12 +18,12 @@ object PullFeed {
    *    String: API key
    */
   def main(args: Array[String]): Unit = {
-      var url = "file://localhost/readme"
-      var expression = "//author/name"
-      var APIkey = "secret"
-   
+    var url = "file://localhost/readme"
+    var expression = "//author/name"
+    var APIkey = "secret"
+
     // TODO Algorithm:
-    // 1. read parameters: url, mapping expression, secret
+    // 1. read parameters: url, mapping expression, user, template
     // 2. read the file from the url
     // 3. map xpath fields to codein fields
     // 4. call the push method of codein with the secret
@@ -34,63 +33,174 @@ object PullFeed {
     // val url = "http://techcamp.hi.inet//statusnet/index.php/rss"
 
     // 1. read parameters: url, mapping expression, secret
-    if ( args.length > 0 ) url= args( 0 )
-    if ( args.length > 1 ) expression = args( 1 )
-    if ( args.length > 2 ) APIkey= args( 2 )
+    val arguments = getArgs(args)
 
-    // Starting the Actor
-    println( "Starting the actor")
-    APIActor.start()
+    // 2.1. Read the XML file
+    var data = XML.loadString(Source.fromURL(arguments("url")) mkString)
 
-    println ( "Reading " + ( url, expression, APIkey))
+/*
+    // Elements    
+    var e= arguments( "expression") split "/" filter (x=> x.length > 0)
+    println( "elementos")
+    e foreach ( x=> { print( "\""); print(x); print( "\""); } )
+    println( "")
     
-    // 2. read the file from the url
-    var data = XML.loadString( Source.fromURL(url).mkString )
+    val r= extractElements( data, e)
+    println( "elements\n+++++++")
+    r foreach println
+    println( "")
+*/
 
-    // 3. map xpath fields to codein fields
-    // 3.1 Get the project name
-    var project = (data \ "id").text split "/"
-    var projectname= project(1) + "/" +project(2)
-    println ( "projectname= " + projectname )
-
-    // 3.2 Get a sequence of XML elements
-    var n= filter(data, expression)
+    val expressions= tokenize( arguments( "expression"))
+    var mapa = new HashMap[ String, Array[String]]
     
-    // 3.3 Process each element
-    // TODO: Use Scala Extractors and Classes to get the data
-    n foreach { 
-        a => {
-            // List: commitID, title, author
-            val entry= List( (a \ "id").text split "/" last, (a \ "title").text, (a \ "author" \ "name").text )
-            // Message body= #projectname~commitID url=commitURL -- description
-            val message= "#" + projectname + "~" + entry(0) + "url=http://github.com/" + projectname + "/commit/" + entry(0) + "--" + entry(1);
-            println( "-----begin")
-            println( message);
-            println( "author=" + entry(2) )
-            println( "-----end")
-            println( "")
-            
-            // Build a PostEntry object
-            val post= new PostEntry( entry(2), message)
-            
-            APIActor ! post
-          }
+    expressions foreach { v => 
+    // Subelements
+        println( "variable= " + v(0))
+        val s= v(1) split "//" filter (x=> x.length > 0)
+        println( "subelementos")
+        s foreach ( x=> { print( "\""); print(x); print( "\" "); } )
+        println( "")    
+        
+        val z= extractSubElements( data, s)
+        println( "sub\n+++++++")
+        mapa += ( v(0)-> (z  map ( x => x.text) ).toArray)
     }
-    // println( "salida")
-    // n map ( _ text) foreach println
-
+    
+    mapa foreach { v=> println( v._1 + " -> "); v._2 foreach println; println("----") }
+    
+    var maximo: Int= 0
+    mapa foreach {  v=> maximo= maximo max v._2.length; println( "max=" + maximo + " ? " + v._2.length) }
+    println ( "maximo=" + maximo)
+    
+    println( "mi mapa")
+    mapa foreach { v=> 
+        val s = getValue( mapa, v._1, v._2.length - 1)
+        println( v._1 + "-> " + s)
+    }
+    
+    val template= arguments( "template")
+    for ( i <- 0 to maximo - 1){
+        var mensaje: String= template
+        
+        for( m <- mapa){
+            val indice= if( m._2.length == 1) 0 else i 
+            val valor= m._2( indice)
+            mensaje= substitute( mensaje, m._1, valor)
+        }
+        println( "mensaje " + i + ": '"+ mensaje + "'")
+        StatusnetAPI.update( arguments( "user"), mensaje, scala.collection.mutable.Map( "source" -> "pullfeed"), "xml");
+    }
+    
   }
   
-  var counter=1;
-  def filter( data: NodeSeq, e: String): NodeSeq ={
-      // Parse expression
-      val tokens= e split "//"
-
-      // counter= counter + 1
-      // println( counter + " tokens=" + (tokens mkString( "", "-", "")) + " data=" + (data toString))
-      if( tokens.length == 1) return data \\ tokens.head
-      filter( data \\ tokens.head, tokens.tail mkString ("", "//", ""))
+  def substitute( template: String, token: String, valor: String) : String =
+  {
+      // println( "template=" + template)
+      // println( "token=" + token)
+      // println( "valor=" + valor)
+      
+      val start= template.indexOf( "{"+token+"}")
+      if( start< 0 ) return template
+      val resultado = template.patch( start, valor, token.length+2 )
+      
+      // println( "sustitucion=" + resultado)
+      return resultado
   }
+
+  def getValue( mapa: Map[String, Array[String]], token: String, order: Int): String = {
+      val row = mapa( token)
+      var indice= order
+      
+      if( row.length == 1)
+        indice= 0
+    
+      return row( indice)
+  }
+  /* 
+  * Get data elements based on expression. Elements are only first level elements
+  * @data XML data
+  * @e expression xpath
+  * @return XML data that matches the expression
+  * */
+
+  def extractElements(data: NodeSeq, e: Array[String]): NodeSeq = {
+    // Parse expression
+    /*
+    println("data=" + data)
+    print("e=")
+    e foreach println
+    println("head=" + e.head)
+    println("tail=" + e.tail)
+    */
+    var resultado= data
+
+    if ( e.length == 1 ) {
+        resultado= data \ e.head
+      }
+      else
+      resultado= extractElements( data \ e.head, e.tail)
+
+    return resultado
+
+  }
+
+
+  /* 
+  * Get data elements based on expression. Elements are only first level elements
+  * @data XML data
+  * @e expression xpath
+  * @return XML data that matches the expression
+  * */
+
+  def extractSubElements(data: NodeSeq, e: Array[String]): NodeSeq = {
+    // Parse expression
+    /*
+    println("data=" + data)
+    print("e=")
+    e foreach println
+    println("head=" + e.head)
+    println("tail=" + e.tail)
+    */
+    var resultado= data
+
+    if( e.head.contains( "/"))
+    {
+        val elements= e.head.split( "/") filter (x=> x.length > 0)
+        if( elements.length == 1)
+            resultado= resultado \\ elements.head
+        else
+            resultado= extractElements( resultado \\ elements.head, elements.tail)
+    }
+    else if ( e.length == 1 ) {
+        resultado= resultado \\ e.head
+      }
+    else
+      resultado= extractSubElements( resultado \\ e.head, e.tail)
+
+    return resultado
+
+  }
+
+  def getArgs(args: Array[String]): Map[String, String] = {
+    val resultado: Map[String, String] = Map(
+      "url" -> "http://example.com/rss",
+      "expression" -> "//entry->content",
+      "user" -> "codein",
+      "APIkey" -> "a1b2c3d4")
+
+    if (args.length > 0) resultado("url") = args(0)
+    if (args.length > 1) resultado("expression") = args(1)
+    if (args.length > 2) resultado("user") = args(2)
+    if (args.length > 3) resultado("template") = args(3)
+    
+    println(args mkString)
+    println(resultado)
+    return resultado
+  }
+  
+  def tokenize(e: String) = e.split(",").map(z => z.split("->"))
+  
 
 }
 
