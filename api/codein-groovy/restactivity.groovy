3ca6b564bc643c903b03618c3f06dc36cdcf7880
miceno <orestes@acm.org>
2010-12-12 23:23:33 +0100

    [MINOR]: Updated file locations

---
+++ api/codein-groovy/restactivity.groovy
@@ -0,0 +1,14 @@
+/**
+ * 
+ * REST interface for the activity stream
+ * 
+ * /activitystream              Site Activity Stream with all the activity of the site
+ * /activitystream/{user}       User Activity Stream
+ * TODO: add pagination or limit the results
+ * 
+ */
+ 
+import org.lpny.groovyrestlet.*
+ 
+GroovyRestlet gr = new GroovyRestlet()
+
