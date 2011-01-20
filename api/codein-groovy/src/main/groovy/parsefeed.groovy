dba4782eec2bc9f09884f9bee49633353187946b
miceno <orestes@acm.org>
2011-01-20 12:09:52 +0100

    [IMPROVEMENT]: parse feed

--- api/codein-groovy/src/main/groovy/parsefeed.groovy
+++ api/codein-groovy/src/main/groovy/parsefeed.groovy
@@ -1,32 +1,26 @@
-package tid
 
 /**
 * A consumer console that consumes all the message in a queue
 */
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-
+import static javax.xml.xpath.XPathConstants.*
 import javax.xml.xpath.*
-
+import groovy.xml.dom.DOMCategory
+import groovy.xml.*
 import org.apache.log4j.*
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import ExpressionContainer
 
 Logger log= Logger.getLogger( getClass().getName())
 
 def cli = new CliBuilder( usage: 'groovy parsefeed' )
 
 cli.h(longOpt: 'help', 'usage information')
-cli.c(argName:'configfile', longOpt:'config', required: true,
-      args: 1, 'Configuration filename')
 cli.x(argName:'xpath', longOpt:'xpath', required: false,
       args: 1, 'xpath expression')
-cli.f(argName:'file', longOpt:'file', required: false,
-      args: 1, 'xml file')
+cli.f(argName:'expressions', longOpt:'expressions', required: true,
+      args: 1, 'expressions file')
+cli.u(argName:'url', longOpt:'url', required: false,
+      args: 1, 'url of the feed')
 
 def opt = cli.parse(args)
 if (!opt) return
@@ -35,48 +29,42 @@ if (opt.h) {
    return null
 }
 
+
+    // A closure that applies each Expression to an element
+    def apply= { listExpressions, element ->
+        listExpressions.collect{ xexpression ->
+            log.debug "begin parsing feed with ${xexpression}"
+            def result
+            use (DOMCategory) { 
+                result= element.xpath( xexpression.XPATH, NODESET) 
+                /* Print each element */
+                log.debug "${xexpression.XPATH}: ${result.size()}= ${result.text()}"
+            }
+            result.text()
+        }// lexpression.collect
+    }
+def parser= new ExpressionContainer( opt.f)
+    // Curry the apply to the Parser
+    def applyToElement= apply.curry( parser.expressions.values())
+
 final String XPATH_EXPRESSION="//entry"
-String xpath=  (opt.x ?: XPATH_EXPRESSION )
 
-File f= new File( opt.f)
-log.debug  "reading file: ${f}" 
+    // xpath=  (opt.x ?: XPATH_EXPRESSION )
+    // doc = new DOMBuilder.parse( opt.u, false, false)
+    doc = DOMBuilder.parse( 
+              new InputStreamReader( opt.u.toURL().openStream()), 
+              false, 
+              false).documentElement
 
-DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-DocumentBuilder db = factory.newDocumentBuilder();
-Document doc = db.parse(f);
+    use( DOMCategory){
+        nodes= doc.xpath( XPATH_EXPRESSION, NODESET)
 
-log.debug "feed begin Text".center( 30, '=')
-log.debug  doc.getDocumentElement().textContent
-log.debug "feed end Text".center( 30, '=')
-
-log.debug "feed begin dump".center( 30, '=')
-log.debug  f.text
-log.debug "feed end dump".center( 20, '=')
-
-def xp = XPathFactory.newInstance().newXPath()
-def xpInner = XPathFactory.newInstance().newXPath()
-def expr     = xp.compile(XPATH_EXPRESSION)
-def nodes    = expr.evaluate(doc, XPathConstants.NODESET)
-
- 
-def lexpression= xpath.split( ",")
-
-    // For each node, extract all the expressions
-    nodes.collect{
-        element ->
-        // For each expression extract data
-        lexpression.collect{ xexpression ->
-           log.debug "begin parsing feed with ${xexpression}"
-           def result= xp.evaluate( xexpression, element)
-
-           /* Print each element */
-           log.debug "${xexpression}: ${result}"
-           result
-        }
-    }.each{ log.debug "texto: " + it }
+        // For each node, extract all the expressions
+        result= nodes.collect( applyToElement)
+        result.each{ log.debug "texto: " + it }
+        result
+    }// use
     
     log.debug "end parsing feed"
 
 System.exit(0)
-
-
