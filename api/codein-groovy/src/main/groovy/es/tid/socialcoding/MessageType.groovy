dc69cc3b4a63fd8580cedff648bbff54e34ed23e
miceno <orestes@acm.org>
2011-01-30 20:34:43 +0100

    [FEATURE]: SOCIALCOD-79

--- api/codein-groovy/src/main/groovy/es/tid/socialcoding/MessageType.groovy
+++ api/codein-groovy/src/main/groovy/es/tid/socialcoding/MessageType.groovy
@@ -3,8 +3,9 @@ package es.tid.socialcoding
 
 
 class MessageType {
-      final static String FEED     = 'FEED'
-      final static String ENTRY    = 'ENTRY'
-      final static String FEED_TASK = 'FEED_TASK'
+      final static String FEED          = 'FEED'
+      final static String ENTRY         = 'ENTRY'
+      final static String FEED_TASK     = 'FEED_TASK'
+      final static String ERROR_MESSAGE = 'ERROR_MESSAGE'
 }
 
