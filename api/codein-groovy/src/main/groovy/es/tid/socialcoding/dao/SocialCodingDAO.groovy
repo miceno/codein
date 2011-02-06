d839d6aa70a8f76b44c087f4c8ff8cfbc8f825d4
miceno <orestes@acm.org>
2011-02-06 17:32:22 +0100

    [REFACTOR]: DAO

--- api/codein-groovy/src/main/groovy/es/tid/socialcoding/dao/SocialCodingDAO.groovy
+++ api/codein-groovy/src/main/groovy/es/tid/socialcoding/dao/SocialCodingDAO.groovy
@@ -21,24 +21,36 @@ package es.tid.socialcoding.dao
  *      }
  */
 
+import org.apache.log4j.Logger
+
 abstract class SocialCodingDAO { 
     def db
-    abstract List getFields()
+    List fields = []
+    List getFields() { fields.size()? fields: init()}
+
+    private log= Logger.getLogger( getClass().getName())
 
     def dataSet()    { db.dataSet(tablename) } 
     def getIdField() { tablename.toLowerCase() + 'Id' } 
     def getWhereId() { "WHERE $idField = ?"}
+    def init(){
+       fields= db.rows('describe '+tablename).collect{ it }
+       log.debug "Fields: $fields"
+    }
+
+    def sanitizeFields( args){
+        List argfields= args.keySet().toList()
+        List intersection= getFieldNames().intersect( argfields)
+        List oddFields= argfields - intersection
+        if( oddFields.size())
+            log.debug "Odd Fields in operation: $oddFields"
+        return args.subMap( intersection)
+    }
 
     String getTablename() { 
         def name = this.getClass().name 
         return name[name.lastIndexOf('.')+1..-4]
     }
-
-    def create(List args) {
-        Map argMap = [:]
-        args.eachWithIndex { arg, i -> argMap[fieldNames[i]] = arg }
-        dataSet().add argMap
-    } 
 
     Map getSchema() {
         Map result = [:] 
@@ -48,21 +60,56 @@ abstract class SocialCodingDAO {
 
     List getFieldNames() {
         List result = [] 
-        0.step(fields.size(),2) { result << fields[it] } 
-        return result
+        return fields*.Field
     }
 
+    def create(Map args) {
+        args= sanitizeFields( args)
+        dataSet().add args
+    } 
 
-    def update(field, newValue, id) {
-        def stmt = "UPDATE $tablename SET $field = ? $whereId" 
-        db.executeUpdate stmt, [newValue, id]
+    private def composeFieldStmt = { k, v -> "$k = '$v'"}
+
+    def update(id, args) {
+        args= sanitizeFields( args)
+        setStmt= args.collect( composeFieldStmt).join( ',')
+        stmt   = "UPDATE $tablename SET $setStmt $whereId" 
+        log.debug( "DAO update: $stmt")
+        db.executeUpdate stmt, [ id]
     } 
 
     def delete(id) {
         def stmt = "DELETE FROM $tablename $whereId" 
+        log.debug( "DAO delete: $stmt")
         db.executeUpdate stmt, [id]
     }
 
+    def findBy( def args){
+        def selects = fieldNames + idField
+        args= sanitizeFields( args)
+        def result=[]
+        def whereStmt = args.collect( composeFieldStmt).join( ' and ')
+        def stmt = "SELECT " + selects.join(',') +
+                   " FROM $tablename WHERE $whereStmt"
+        log.debug( "DAO findBy: $stmt")
+        db.eachRow(stmt.toString()){ rs -> 
+            Map businessObject = [:] 
+            selects.each { businessObject[it] = rs[it] } 
+            result << businessObject
+        }
+        log.debug "FindBy: result = $result"
+        return result
+    }
+
+    def find(id){
+        def selects = fieldNames + idField
+        def stmt = "SELECT " + selects.join(',') +
+                   " FROM $tablename $whereId"
+        log.debug( "DAO find: $stmt")
+        def result= db.firstRow( stmt.toString(), [id])
+        log.debug "Find: result = $result"
+        return result
+    }
 
     def all(sortField) {
         def selects = fieldNames + idField 
@@ -76,4 +123,5 @@ abstract class SocialCodingDAO {
         } 
         return result
     }
+
 }
