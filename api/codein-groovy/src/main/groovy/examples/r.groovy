99e90dbc4cb717883541df46ef4b972083dd6505
miceno <orestes@acm.org>
2011-02-08 11:41:31 +0100

    [IMPROVEMENT]: Restlet example with Resource subclasses

--- api/codein-groovy/src/main/groovy/examples/r.groovy
+++ api/codein-groovy/src/main/groovy/examples/r.groovy
@@ -18,52 +18,7 @@ import org.restlet.resource.DomRepresentation;
 import org.restlet.resource.Representation;  
 import org.restlet.resource.ResourceException;  
 import org.restlet.resource.Variant;  
-
-class UsersResource extends Resource
-{
-     UsersResource(Context context, Request request, Response response) {  
-        super(context, request, response);  
-  
-        // Get the "itemName" attribute value taken from the URI template  
-        // /items/{itemName}.  
-        this.domain = (String) getRequest().getAttributes().get("domain");  
-  
-        // Define the supported variant.  
-        getVariants().add(new Variant(MediaType.TEXT_XML));  
-
-        // By default a resource cannot be updated.  
-        setReadable(true);  
-
-        // By default a resource cannot be updated.  
-        setModifiable(false);  
-    }  
-
-    /** 
-     * Returns a listing of all registered items. 
-     */  
-    Representation represent(Variant variant) throws ResourceException {  
-        // Generate the right representation according to its media type.  
-        if (MediaType.TEXT_XML.equals(variant.getMediaType())) {  
-            try {  
-                DomRepresentation representation = new DomRepresentation(  
-                        MediaType.TEXT_XML);  
-                // Generate a DOM document representing the list of  
-                // items.  
-                
-                representation.setDocument( d);  
-
-                // Returns the XML representation of this document.  
-                return representation;  
-            } catch (IOException e) {  
-                e.printStackTrace();  
-            }  
-        }  
-  
-        return null;  
-    }  
-
-
-}
+import org.apache.log4j.Logger
 
 String script= '''
 
@@ -83,8 +38,8 @@ def config= SocialCodingConfig.newInstance().config
 
 final Integer PORT=9999
 
-// Create UserFeedDAO
-def database= new DbHelper().db
+class UserResource{
+}
 
 builder.component{
     current.servers.add(protocol.HTTP, PORT)
@@ -92,7 +47,7 @@ builder.component{
     application(uri:"/socialcoding"){
         router{
             // a list of all users
-            resource("/user", ofClass:UsersResource)
+            resource( uri:"/user", ofClass:UsersResource.class)
             // a list of domain users
             resource( uri:"/user/{domain}",        ofClass:UsersResource)
             // The add a user
@@ -107,3 +62,4 @@ InputStream is = new ByteArrayInputStream(script.getBytes("UTF-8"));
 
 gr= new GroovyRestlet()
 gr.build( is)
+
