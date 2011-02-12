5f367799546475ed804ff3eab07f4fc9765e1256
miceno <orestes@acm.org>
2011-02-12 11:13:19 +0100

    [REFACTOR]: Unique Backend

--- api/codein-groovy/src/main/groovy/restactivity.groovy
+++ api/codein-groovy/src/main/groovy/restactivity.groovy
@@ -32,52 +32,6 @@ final Integer PORT=config.rest.activity.port
 // Create Database Connection
 def db= new DbHelper().db
 
-// List of all entries
-def listHandle = { title,entries,req,resp->
-    log.debug 'Listing -> ' + title
-    
-String result= SocialCodingAtomGenerator.generateEntries( entries, title, title)
-
-    log.debug( "listado= ${result}")
-    resp.setEntity( result,
-                    MediaType.APPLICATION_ATOM_XML
-                  )
-    resp.setStatus( Status.SUCCESS_OK)
-}
-
-// Closure for the list of all entries
-def listAllActivity= listHandle.curry( "All", db.dataSet('Entry').findAll())
-
-// Closure for the list of users
-def listUserActivity= { req,resp->
-    
-    user= req.attributes.get( 'user')
-    domain= req.attributes.get( 'domain')
-    log.debug "user $user , domain $domain"
-
-    // findAll has a bug that prevents it using a variable in the expression
-    // findAll{ it.ownerId == user } will not work
-    // findAll{ it.ownerId == 'orestes'} will work
-    // So I decided to change to directly querying the database
-String filterTmpl= """
-  select * from Entry 
-  where 
-    ownerId = '$user' 
-    && ownerDomain= '$domain'
-"""
-
-List userEntries= db.rows( filterTmpl)
-
-    if ( userEntries.size())
-    {
-        listUser= listHandle.curry( "User $user of domain $domain",  
-                                    userEntries)
-        listUser.call( req, resp)
-    }
-    else
-        resp.setEntity( "No hay datos", MediaType.TEXT_HTML)
-}
-
 Router r
 builder.component{
     current.servers.add(protocol.HTTP, PORT)
