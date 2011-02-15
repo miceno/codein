95ac49588b64353d0dad67eee3bf17bfaf57c941
miceno <orestes@acm.org>
2011-02-15 23:19:35 +0100

    [UPDATE]: updated escaping XML

--- api/codein-groovy/src/main/groovy/examples/t.groovy
+++ api/codein-groovy/src/main/groovy/examples/t.groovy
@@ -3,14 +3,14 @@ import java.net.URLEncoder
 import org.apache.commons.lang.StringEscapeUtils
 
 
-String prueba= "<name>hola</name>"
+String prueba= "<name>holaáéíóúñ</name>"
 
 def builder = new groovy.xml.MarkupBuilder()  // construct a builder step(1)
 
 String codec= URLEncoder.encode( prueba, "UTF-8")
-String escapeHtml= StringEscapeUtils.escapeXml( "EspaÃ±a SÃ¡nchez JÃ³rgen")
+String escapeHtml= "España Sánchez Jörgen"
 
-def escapeClosureBuilder= { b, text->  b.mkp.yieldUnescaped( text) }
+def escapeClosureBuilder= { b, text->  b.mkp.yieldUnescaped( StringEscapeUtils.escapeXml(text)) }
 def escape= escapeClosureBuilder.curry( builder)
    
 def xml=   builder.html{
@@ -20,6 +20,8 @@ def xml=   builder.html{
      codificadohtml { mkp.yieldUnescaped( escapeHtml) }
 //     codificadohtml { escapeClosure( escapeHtml) }
      codificadohtml { escape( escapeHtml) }
+     codificadohtml { escape( prueba) }
+     codificadohtml prueba
 //     codificadourlencoder { mkp.yield( codec )}
    }
 
