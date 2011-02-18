5b83a516a6d003b305d90230e6ccba5f54279028
miceno <orestes@acm.org>
2011-02-18 14:24:30 +0100

    [UPDATED]: updated options and parameters

--- api/codein-groovy/src/main/groovy/testcurl.sh
+++ api/codein-groovy/src/main/groovy/testcurl.sh
@@ -1,1 +1,2 @@
-curl --data-urlencode payload@data.json -H "Referer: http://github.com" http://localhost:8181/socialcoding/push/github/1234
+URL='http://localhost:8181/socialcoding/push/github/1234'
+curl -v -i --data-urlencode payload@data.json "$URL"
